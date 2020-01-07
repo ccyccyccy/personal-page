039767fa9242a9a341a20bb83451a07f0850d36d
Chee Yuan <chong-1997@hotmail.com>
2020-01-07 17:16:08 +0800

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import Sudoku from './Sudoku';
+
 function App() {
   return (
     <div>
-      <h1>Hello World!!!!</h1>
-      <h1>Test Test~~~</h1>
+      <h1>Hello ~~~~~~~~~~~~~~~~ World!!!!</h1>
+      <h1>This is the personal page for Chong Chee Yuan</h1>
       <a href="https://github.com/ccyccyccy/">This is my GitHub</a>
+      <Sudoku />
     </div>
   );
 }
