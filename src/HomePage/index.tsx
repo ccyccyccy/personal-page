2fff934bcb3bb47080ad69d9e0ff377812d3461f
Chee Yuan <chong-1997@hotmail.com>
2020-02-23 01:54:52 +0800

    test

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => (
   <div>
-    <h1>Hello ~~~~~~!!!!!~~~~~ World!!!!</h1>
+    <h1>Hello ~~~~~~!!!! !~~~~~ World!!!!</h1>
     <h1>This is the personal page for Chong Chee Yuan</h1>
     <a href="https://github.com/ccyccyccy/">This is my GitHub</a>
   </div>
